869e86186343573426a0845c80b7fa3a530c0028
Matthew McPeak <mcpeakml@etsu.edu>
2020-02-07 07:18:29 +0000

    Irrelevant change to Test Jenkins

--- BikeShopAnalytics/Program.cs
+++ BikeShopAnalytics/Program.cs
@@ -21,6 +21,7 @@ namespace BikeShopAnalytics
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    var irrelevant = "meh";
                 });
     }
 }
