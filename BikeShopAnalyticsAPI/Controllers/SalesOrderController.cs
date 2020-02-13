5e0e7d70f07fb623350f10f0353ead77fdc5a57f
Matthew L. McPeak <McPeakML@etsu.edu>
2020-02-13 09:41:42 -0500

    Added API Endpoints.

---
+++ BikeShopAnalyticsAPI/Controllers/SalesOrderController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeShopAnalyticsAPI.Models.Entities;
+using BikeShopAnalyticsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BikeShopAnalytics.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesOrderController : ControllerBase
+    {
+        private IRepository<SalesOrder> _salesOrderRepo;
+
+        public SalesOrderController(IRepository<SalesOrder> salesOrderRepo)
+        {
+            _salesOrderRepo = salesOrderRepo;
+        }
+
+
+        [HttpPost("[action]")]
+        public IActionResult Create(SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _salesOrderRepo.Create(salesOrder);
+                return Ok("Success! Sales Order Created!");
+            }
+            return Problem("Error! Could not create the sales order..");
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Update(SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _salesOrderRepo.Update(salesOrder);
+                return Ok("Success! Sales Order Updated!");
+            }
+            return Problem("Error! Could not update the sales order..");
+        }
+    }
+}
---
+++ BikeShopAnalyticsAPI/Services/IRepository.cs
@@ -0,0 +1,55 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:  ProMan/ProMan
+//	File Name:         IRepository.cs
+//	Course:            CSCI 3110-001 - Advanced Web Dev & Design
+//	Author:            Matthew McPeak, McPeakML@etsu.edu, East Tennessee State University
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace BikeShopAnalyticsAPI.Services
+{
+    /// <summary>
+    /// An Abstract Repository for ProjectDbContext
+    /// </summary>
+    /// <typeparam name="TEntity">Object Type</typeparam>
+    public interface IRepository<TEntity>
+    {
+        /// <summary>
+        /// Reads a TEntity from the Db, with a comparing Function and any sub-entities to include. 
+        /// </summary>
+        /// <param name="predicate">A Comparing Function in the form of a lambda</param>
+        /// <param name="includes">An array of lambda expressions for sub-entities to be included</param>
+        /// <returns>A TEntity</returns>
+        TEntity Read(Func<TEntity, bool> predicate, params Expression<Func<TEntity, object>>[] includes);
+
+        /// <summary>
+        /// Reads The Entire table of TEntity, with any sub-entities to include. 
+        /// </summary>
+        /// <param name="includes">An array of lambda expressions for sub-entities to be included</param>
+        /// <returns>An IQueryable of TEntity</returns>
+        IQueryable<TEntity> ReadAll(params Expression<Func<TEntity, object>>[] includes);
+
+        /// <summary>
+        /// Creates a TEntity in the table of Type of TEntity
+        /// </summary>
+        /// <param name="obj">A object of TEntity</param>
+        /// <returns>A TEntity</returns>
+        TEntity Create(TEntity obj);
+
+        /// <summary>
+        /// Updates a TEntity in the table of Type of TEntity
+        /// </summary>
+        /// <param name="obj">A object of TEntity</param>
+        void Update(TEntity obj);
+
+        /// <summary>
+        /// Deletes a TEntity in the table of Type of TEntity
+        /// </summary>
+        /// <param name="obj">A object of TEntity</param>
+        void Delete(TEntity obj);
+    }
+}
