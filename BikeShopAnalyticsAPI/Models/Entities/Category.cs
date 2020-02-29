a6c0c7ec7b52413000ba0599827f89ec3b695d2c
Zachery Johnson <johnsonzd@etsu.edu>
2020-02-29 10:26:50 -0500

    Created Category Entity

---
+++ BikeShopAnalyticsAPI/Models/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShopAnalyticsAPI.Models.Entities
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string PlotItemOne { get; set; }
+
+        public string PlotItemTwo { get; set; }
+
+
+    }
+}
