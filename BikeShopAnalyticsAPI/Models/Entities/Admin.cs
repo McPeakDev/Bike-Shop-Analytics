2f911d054646550da6207a63d15a3a84685278e7
Zachery <johnsonzd@etsu.edu>
2020-03-03 16:00:36 -0500

    Added Admin classes, updated category with new fields

---
+++ BikeShopAnalyticsAPI/Models/Entities/Admin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BikeShopAnalyticsAPI.Models.Entities
+{
+    public class Admin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AdminID { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string FirstName { get; set; }
+        [StringLength(150)]
+        public string MiddleName { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string LastName { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        [StringLength(10)]
+        public string UserName { get; set; }
+            
+        
+    }
+}
