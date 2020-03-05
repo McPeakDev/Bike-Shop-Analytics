a951fc1586b67503e4c55ec24d4360d8f9f8ae41
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2020-03-05 16:03:06 -0500

    Bug with JSON fetch - can't wait for it

--- bikeshop-user-frontend/src/App.js
+++ bikeshop-user-frontend/src/App.js
@@ -1,19 +1,17 @@
 import React, {Component} from "react"
 import logo from "./logo.svg";
 import "./App.css"
-import Comparisons from './Components/Comparisons'
+import Api from './APIClient'
 
 class App extends Component {
   constructor(props)
   {
     super(props)
   }
+ 
   render(){
     return(
-      <div>
-        <Comparisons></Comparisons>
-      </div>
-      
+      <Api></Api>
     )
   }
 }
---
+++ bikeshop-user-frontend/src/json-table.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function jsonTable(props)
+{
+    return (
+        <table>
+            <thead>
+                <tr>
+
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    )
+}
