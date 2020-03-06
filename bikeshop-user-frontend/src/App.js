6e2908041ffd242e0f9c756725d38a514f23f9f4
Matthew McPeak <McPeakML@etsu.edu>
2020-03-05 21:01:08 -0500

    Dynamic Bike Data works in user-frontend

--- bikeshop-user-frontend/src/App.js
+++ bikeshop-user-frontend/src/App.js
@@ -1,17 +1,42 @@
 import React, {Component} from "react"
 import logo from "./logo.svg";
 import "./App.css"
-import Api from "./APIClient"
+import Table from "./Table"
+import API from './APIClient'
+
 
 class App extends Component {
   constructor(props)
   {
     super(props)
+    this.state = {data: [], obj: {}};
+    this.change = this.change.bind(this);
   }
+  async componentDidMount() 
+  {
+      let api = new API();
+      this.setState({ data: await api.get("bike", "readall")});   
+      this.setState(() => ({obj: this.state.data[0]}));   
+      console.log(this.state.obj);
+
+  }
+
+  change(event) 
+  {
+    console.log(event.value)
+    this.setState(() => ({obj: this.state.data[event.value]}));
+    this.forceUpdate();
+  }
+
   render(){
+    let i = 0;
+    let optionItems = this.state.data.map((item) => 
+            <option value={i++} key={item.bikeID} >{item.name}</option>
+        );
     return(
       <div>
-        <Api />
+        <select ref="bikes" onChange={e => this.change(e.target)}>{optionItems}</select>
+        <Table selectedObj={this.state.obj}/>
       </div>
     )
   }
--- bikeshop-user-frontend/src/Table.js
+++ bikeshop-user-frontend/src/Table.js
@@ -1,45 +1,71 @@
 import React from 'react'
 import './Table.css'
 
+
 export default class Table extends React.Component {
+
     constructor(props)
     {
         super(props);
+        console.log(props)
+        this.state = {data: [] };
         this.getHeader = this.getHeader.bind(this);
         this.getRowsData = this.getRowsData.bind(this);
         this.getKeys = this.getKeys.bind(this);
     }
 
-    getKeys = function() {
-        return Object.keys(this.props.data[0])
+    getKeys() 
+    {
+        return Object.keys(this.props.selectedObj)
     }
 
-    getHeader = function() {
+    getHeader() 
+    {
         var keys = this.getKeys();
         return keys.map((key, index)=> {
-            return <th key={key}>{key}</th>
+            return <th key={key}>{key.capitalize()}</th>
         })
     }
 
-    getRowsData = function() {
-        var items = this.props.data;
+    getRowsData() 
+    {
+        var items = this.props.selectedObj;
+        console.log(items);
         var keys = this.getKeys();
-        return items.map((row, index)=> {
-            return <tr key={index}><RenderRow key={index} data={row} keys={keys} /></tr>
-        })
+        let rmp = keys.map(k => {
+            console.log(items[k].toString())
+            return <tr key={k}><RenderRow key={k}/></tr>
+
+        });
+        console.log(rmp);
+
     }
 
     render() {
+        var keys = this.getKeys();
         return (
             <div>
                 <table>
                     <thead>
                         <tr>
-                            {this.getHeader()}
+                            {
+                                keys.map((key)=> {
+                                    return <th key={key}>{key.capitalize()}</th>
+                                })
+                            }
                         </tr>
                     </thead>
                     <tbody>
-                        {this.getRowsData()}
+                        <tr>
+                        {
+                            keys.map(k => {
+                                console.log(this.props.selectedObj[k].toString())
+                                return <td key={k}>{this.props.selectedObj[k].toString()}</td>
+                    
+                            })
+                        }
+                        </tr>
+
                     </tbody>
                 </table>
             </div>
@@ -47,8 +73,12 @@ export default class Table extends React.Component {
     }
 }
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 const RenderRow = (props) => {
-    return props.keys.map((key, index)=> {
-        return <td key={props.data[key]}>{props.data[key]}</td>
+    return props.keys.map((key)=> {
+        return <td key={key}>{props.selectedObj[key]}</td>
     })
 }
