6748b4f8bf67d6d176e707c1589302e663d9b0fe
Matthew McPeak <McPeakML@etsu.edu>
2020-03-31 11:51:09 -0400

    Possible Final Proxy Fix.

--- bikeshop-admin-frontend/src/index.js
+++ bikeshop-admin-frontend/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+<HashRouter>
+    <Route path="*">
+        <App />
+    </Route>
+</HashRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
